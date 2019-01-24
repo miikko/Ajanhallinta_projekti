472ef631e70a444cee7434c6c8f5bdd14e165759
Miikka Oksanen <miikka.oksanen@pp1.inet.fi>
2019-01-24 17:26:35 +0200

    Developer branch initialization

--- Taustapalvelu/service/ServiceMain.java
+++ Taustapalvelu/service/ServiceMain.java
@@ -5,6 +5,7 @@ public class ServiceMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("This is a developer-branch commit.");
 	}
 
 }
