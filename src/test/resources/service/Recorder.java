a5068618bc08663598e94562f7564df3a7bde691
Miikka Oksanen <miikka.oksanen@pp1.inet.fi>
2019-02-07 15:46:27 +0200

    Added modified pom.xml

---
+++ src/test/resources/service/Recorder.java
@@ -0,0 +1,125 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+import com.sun.jna.ptr.IntByReference;
+
+import sun.awt.shell.ShellFolder;
+
+public class Recorder extends Thread {
+	private final int MAX_NAME_LENGTH = 1024;
+	private final Kernel32 KERNEL32 = Kernel32.INSTANCE;
+	private final User32 USER32 = User32.INSTANCE;
+	private boolean quit = false;
+
+	public Recorder() {
+
+	}
+	
+	public void run() {
+		while (!quit) {
+			
+		}
+	}
+
+	public void quit() {
+		quit = true;
+	}
+	
+	public int simpleSum() {
+		return 1 + 1;
+	}
+	
+	// Returns the active window's full name
+	public String getActiveWindowName() {
+		char[] buffer = new char[MAX_NAME_LENGTH];
+		USER32.GetWindowText(USER32.GetForegroundWindow(), buffer, MAX_NAME_LENGTH);
+		return Native.toString(buffer);
+	}
+
+	// Returns the path to the .exe file belonging to the active window
+	public String getActiveWindowFilePath() {
+		HWND hWnd = USER32.GetForegroundWindow();
+		IntByReference processId = new IntByReference();
+		USER32.GetWindowThreadProcessId(hWnd, processId);
+		HANDLE processHandle = KERNEL32.OpenProcess(Kernel32.PROCESS_QUERY_LIMITED_INFORMATION, false,
+				processId.getValue());
+		char[] buffer = new char[4096];
+		IntByReference bufferSize = new IntByReference(buffer.length);
+		boolean success = KERNEL32.QueryFullProcessImageName(processHandle, 0, buffer, bufferSize);
+		KERNEL32.CloseHandle(processHandle);
+		if (!success) {
+			return null;
+		}
+		String fullPath = Native.toString(buffer);
+		return fullPath;
+	}
+
+	//Returns the currently active program's description
+	//Program description can be found by right-clicking the file in File-explorer and selecting properties
+	//Or alternatively it can be found in the Task-Manager's "Name"-column
+	public String getActiveProgramDescription() {
+		String description = null;
+		String path = getActiveWindowFilePath();
+		String exeName = path.split("\\\\")[path.split("\\\\").length - 1];
+		if (exeName != null) {
+			// Remove the '.exe' ending from the name
+			StringBuilder sb = new StringBuilder(exeName);
+			sb.delete(exeName.length() - 4, exeName.length());
+			String fileName = sb.toString();
+			String command = "powershell.exe  get-process " + fileName + " | select-object description";
+			BufferedReader br = null;
+			try {
+				Process powerShellProcess = Runtime.getRuntime().exec(command);
+				powerShellProcess.getOutputStream().close();
+				String line;
+				int lineCounter = 0;
+				br = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+				while ((line = br.readLine()) != null) {
+					lineCounter++;
+					if (lineCounter == 4 && !line.equals("")) {
+						description = line;
+						break;
+					}
+				}
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (br != null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return description;
+	}
+	
+	public Icon getActiveWindowIcon() {
+		String path = getActiveWindowFilePath();
+		File file = new File(path);
+		Icon icon = null;
+		try {
+			ShellFolder shellFolder = ShellFolder.getShellFolder(file);
+			icon = new ImageIcon(shellFolder.getIcon(true));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return icon;
+	}
+}
