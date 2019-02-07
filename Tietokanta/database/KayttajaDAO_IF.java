fd0b53e31c0190a09d6bf6cfb115258e318810b9
Jani_Paavilainen <janimp@metropolia.fi>
2019-02-07 15:04:45 +0200

    Tietokanta
    Tietokanta peruskäyttäjä objectilla

---
+++ Tietokanta/database/KayttajaDAO_IF.java
@@ -0,0 +1,9 @@
+package database;
+
+public interface KayttajaDAO_IF {
+	public abstract boolean createKayttaja(Kayttaja kayttaja);
+	public abstract Kayttaja readKayttaja(String user_name);
+	public abstract Kayttaja[] readKayttajat();
+	public abstract boolean updateKayttaja(Kayttaja kayttaja);
+	public abstract boolean deleteKayttaja(String user_name);
+}
